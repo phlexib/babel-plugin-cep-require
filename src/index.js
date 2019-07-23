abfd46b513746f49d52296d64ca290b6605ec2fe
Benjamin Rohel <ben.rohel@gmail.com>
2019-07-23 08:55:01 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+export default function(babel) {
+  const { types: t } = babel;
+
+  return {
+    name: "transformRequire",
+    visitor: {
+      VariableDeclaration(path, state) {
+        if (path.node.leadingComments) {
+          if (path.node.leadingComments[0].value == "@ts-ignore-cep") {
+            console.log(state.opts);
+            path.node.declarations[0].init.callee.name =
+              "window.cep_node.require";
+          }
+        }
+      }
+    }
+  };
+}
---
+++ tests/transform.test.js
@@ -0,0 +1,17 @@
+import { transform } from "@babel/core";
+import * as babel from "@babel/core";
+import * as transformRequire from "../src/index";
+
+const example = `
+//@ts-ignore-cep
+const fs = require("fs");
+//@ts-ignore-cep
+const path = require("path");
+//@ts-ignore
+const dateFns = require("date-fns");
+`;
+
+it("first 2 requires to be replace with window.cep_node.require", () => {
+  const { code } = babel.transform(example, { plugins: [transformRequire] });
+  expect(code).toMatchSnapshot();
+});
